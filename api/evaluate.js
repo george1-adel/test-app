738b8b52005047d44441e6d5909640c4e9f86338
george <georgeadel502@gmail.com>
2025-12-16 08:18:19 +0200

    Update evaluate.js

--- api/evaluate.js
+++ api/evaluate.js
@@ -53,7 +53,8 @@ export default async function handler(req, res) {
         `;
 
         // الاتصال بـ Gemini API
-        const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-exp:generateContent?key=${apiKey}`;
+        
+       const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`;
 
         const response = await fetch(url, {
             method: 'POST',
